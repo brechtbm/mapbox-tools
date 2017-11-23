7fef68b8a6bb6f9034aa7079ff5dc23c21dcafc0
stefda <stefda@gmail.com>
2017-11-23 00:42:32 +0000

    feat: add upload

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,31 @@ FROM alpine:3.6
 
 MAINTAINER David Stefan <stefda@gmail.com>
 
-# add required dependencies
-RUN apk add --update bash openssl g++ make sqlite-dev zlib-dev
+# add tippecanoe dependencies
+RUN apk --no-cache update && \
+    apk --no-cache add bash openssl g++ make sqlite-dev zlib-dev
 
-# dowwnload and install tippecanoe
-RUN wget https://github.com/mapbox/tippecanoe/archive/1.27.0.tar.gz && tar xzvf 1.27.0.tar.gz
-WORKDIR /tippecanoe-1.27.0
-RUN make && make install
+# install tippecanoe
+ENV TIPPECANOE_VERSION 1.27.0
 
-# remove dependencies that are no longer needed
-RUN apk del --purge bash openssl make
+RUN wget https://github.com/mapbox/tippecanoe/archive/${TIPPECANOE_VERSION}.tar.gz && \
+    tar xzvf ${TIPPECANOE_VERSION}.tar.gz && \
+    cd /tippecanoe-${TIPPECANOE_VERSION} && \
+    make && \
+    make install
+
+# install aws cli
+ENV AWS_CLI_VERSION 1.11.188
+
+RUN apk --no-cache update && \
+    apk --no-cache add python py-pip py-setuptools ca-certificates groff less && \
+    pip --no-cache-dir install awscli==${AWS_CLI_VERSION} && \
+    rm -rf /var/cache/apk/*
+
+# add upload dependencies
+RUN apk --no-cache update && \
+    apk --no-cache add curl jq
+
+ADD upload.sh /bin/upload
 
 WORKDIR /workdir
---
+++ upload.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# require file and tileset name for arguments
+if [ "$#" -ne 2 ]; then
+    echo "Upload needs a path to the tileset and its name, e.g. 'upload data/czechia.mbtiles czechia'"
+	exit 1
+fi
+
+# require the mapbox username and token to be provided via variables
+if [ -z "$MAPBOX_USERNAME" ] || [ -z "$MAPBOX_TOKEN" ]; then
+	echo "MAPBOX_USERNAME and MAPBOX_TOKEN muset be set"
+	exit 1
+fi
+
+# get temporary AWS credentials to stage the dataset as described in https://www.mapbox.com/api-documentation/#retrieve-s3-credentials
+CREDS=$(curl -X POST "https://api.mapbox.com/uploads/v1/${MAPBOX_USERNAME}/credentials?access_token=${MAPBOX_TOKEN}")
+
+# export credentials into env variables
+export AWS_ACCESS_KEY_ID=$(echo ${CREDS} | jq -r '.accessKeyId')
+export AWS_SECRET_ACCESS_KEY=$(echo ${CREDS} | jq -r '.secretAccessKey')
+export AWS_SESSION_TOKEN=$(echo ${CREDS} | jq -r '.sessionToken')
+
+BUCKET=$(echo ${CREDS} | jq -r '.bucket')
+KEY=$(echo ${CREDS} | jq -r '.key')
+
+# upload file to AWS using retrieved credentials, see https://www.mapbox.com/help/upload-curl/ for details
+aws s3 cp $1 s3://${BUCKET}/${KEY} --region us-east-1
+
+# initiate upload to Mapbox
+UPLOAD=$(curl -X POST -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d '{
+  "url": "http://'${BUCKET}'.s3.amazonaws.com/'${KEY}'",
+  "tileset": "'${MAPBOX_USERNAME}'.'$2'",
+  "name": "'$2'"
+}' 'https://api.mapbox.com/uploads/v1/'${MAPBOX_USERNAME}'?access_token='${MAPBOX_TOKEN}'')
+
+# extract the upload id to query progress later
+UPLOAD_ID=$(echo $UPLOAD | jq -r '.id')
+
+# query upload progress until 'done'
+PROGRESS_STRING='.'
+echo $PROGRESS_STRING
+
+DONE=0
+until [ "$DONE" -eq 1 ]; do
+	RESPONSE=$(curl -s "https://api.mapbox.com/uploads/v1/${MAPBOX_USERNAME}/${UPLOAD_ID}?access_token=${MAPBOX_TOKEN}")
+	DONE=$(echo ${RESPONSE} | jq '.progress')
+	if [ "$DONE" -eq 0 ]; then echo -en "\e[1A"; echo -e "\e[0K\r${PROGRESS_STRING}"; PROGRESS_STRING+='.'; sleep 1; else echo -en "\e[1A"; echo -e "\e[0K\rdone"; fi
+done
